02ba72a2fa93faf17501f19b9cd046dffce4c0f7
aRROBBa <rvalenciab11@gmail.com>
2025-10-24 19:46:15 -0600

    test husky

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+/* global test, expect */
+import '@testing-library/jest-dom'
+import {render,screen} from '@testing-library/react'
+
+test('render',() => {
+  render(<h1>Hola Mundo!</h1>)
+  expect(screen.getByText('Hola Mundo!')).toBeInTheDocument()
+})
